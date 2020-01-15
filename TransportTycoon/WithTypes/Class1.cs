08b7d35e43e090795bc7352e8c5165780c76473c
Thomas Eyde <thomas.eyde@sonat.no>
2020-01-15 16:09:40 +0100

    Implement each transition from factory to port. Notice:
    - Design smell as all state types and tests need to change, when really only the last changes

--- TransportTycoon/WithTypes/Class1.cs
+++ TransportTycoon/WithTypes/Class1.cs
@@ -1,4 +1,9 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TransportTycoon.Time;
+using Xunit;
 
 namespace TransportTycoon.WithTypes
 {
@@ -29,16 +34,16 @@ namespace TransportTycoon.WithTypes
          */
 
         [Fact]
-        public void State_transisions()
+        public void State_transitions()
         {
             var factory = new Factory();
+            var port = new Port();
 
-            var loading = new Loading(factory);
+            factory.Produce(new Container());
 
+            var loading = new Loading(factory, port);
             var moving = loading.Transit();
-
             var unloading = moving.Transit();
-
             var returning = unloading.Transit();
 
             Assert.IsType<Loading>(returning.Transit());
@@ -47,84 +52,170 @@ namespace TransportTycoon.WithTypes
         [Fact]
         public void Load_one_container_from_factory()
         {
+            var container = new Container();
             var factory = new Factory();
-
-            var loading = new Loading(factory);
-
-            var moving = loading.Transit();
+            var port = new Port();
+            var loading = new Loading(factory, port);
+            
+            factory.Produce(container);
+            var moving = (Moving) loading.Transit();
 
             Assert.NotNull(moving.Container);
+        }
+
+        [Fact]
+        public void Load_from_empty_factory()
+        {
+            var factory = new Factory();
+            var port = new Port();
+            var loading = new Loading(factory, port);
+            var next = loading.Transit();
+
+            Assert.IsType<Loading>(next);
+        }
+
+        [Fact]
+        public void Move_container()
+        {
+            var container = new Container();
+            var factory = new Factory();
+            var port = new Port();
+            var loading = new Loading(factory, port);
+            factory.Produce(container);
+
+            var moving = (Moving) loading.Transit();
+
+            Assert.Equal(Moment.From(1), moving.ArrivalTime);
+        }
+
+        [Fact]
+        public void Unload_container_to_port()
+        {
+            var factory = new Factory();
+            var port = new Port();
+
+            factory.Produce(new Container());
+
+            var loading = new Loading(factory, port);
+            var moving = loading.Transit();
+            var unloading = moving.Transit();
+            unloading.Transit();
+
+            Assert.Single(port.Inventory);
+        }
+    }
+
+    public class Port {
+        public IEnumerable<Container> Inventory => inventory;
+        readonly Queue<Container> inventory = new Queue<Container>();
+
+        public void Replenish(Container container)
+        {
+            inventory.Enqueue(container);
         }
     }
 
     public class Factory
     {
-        public Container LoadContainer()
+        public void Produce(Container container)
         {
-            return new Container();
+            inventory.Enqueue(container);
         }
+
+        public T LoadContainer<T>(Func<Container, T> one, Func<T> none)
+        {
+            return inventory.Any() 
+                ? one(inventory.Dequeue()) 
+                : none();
+        }
+
+        readonly Queue<Container> inventory = new Queue<Container>();
     }
 
-    public class Loading
+    public abstract class State {
+        public abstract State Transit();
+    }
+
+    public class Loading : State
     {
         readonly Factory factory;
+        Port port;
 
-        public Loading(Factory factory)
+        public Loading(Factory factory, Port port)
         {
             this.factory = factory;
+            this.port = port;
         }
 
-        public Moving Transit()
+        public override State Transit()
         {
-            return new Moving(factory.LoadContainer(), factory);
+            return factory.LoadContainer<State>(Move, Stay);
         }
+
+        Moving Move(Container container)
+        {
+            var arrivalTime = Moment.From(1);
+            return new Moving(container, factory, port, arrivalTime);
+        }
+
+        Loading Stay() => this;
     }
 
-    public class Moving
+    public class Moving : State
     {
         readonly Factory factory;
+        Port port;
 
-        public Moving(Container container, Factory factory)
+        public Moving(Container container, Factory factory, Port port, Moment arrivalTime)
         {
             Container = container;
             this.factory = factory;
+            ArrivalTime = arrivalTime;
+            this.port = port;
         }
 
-        public Unloading Transit()
+        public override State Transit()
         {
-            return new Unloading(factory);
+            return new Unloading(factory, port);
         }
 
         public Container Container { get; }
+        public Moment ArrivalTime { get; }
     }
 
-    public class Unloading
+    public class Unloading : State
     {
         readonly Factory factory;
+        Port port;
 
-        public Unloading(Factory factory)
+        public Unloading(Factory factory, Port port)
         {
             this.factory = factory;
+            this.port = port;
         }
 
-        public Returning Transit()
+        public override State Transit()
         {
-            return new Returning(factory);
+            port.Replenish(new Container());
+
+            return new Returning(factory, port);
         }
     }
 
-    public class Returning
+    public class Returning : State
     {
         readonly Factory factory;
+        Port port;
 
-        public Returning(Factory factory)
+        public Returning(Factory factory, Port port)
         {
             this.factory = factory;
+            this.port = port;
         }
 
-        public Loading Transit()
+        public override State Transit()
         {
-            return new Loading(factory);
+            return new Loading(factory, port);
         }
     }
 
